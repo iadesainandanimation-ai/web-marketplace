ebd089c5608c652cc1a26b213d9eb750bc23a261
iadesainandanimation-ai <iadesainandanimation@gmail.com>
2025-10-18 21:09:31 +0700

    Delete mockapi.js

--- mockapi.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  try {
-    const response = await fetch("https://68f37e60fd14a9fcc428f201.mockapi.io/products");
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (err) {
-    res.status(500).json({ error: "Gagal mengambil data", detail: err.message });
-  }
-}
